dc339a34bf67f8ca1e5367f3cf492a5fe0d823b0
nanayunn <58680504+nanayunn@users.noreply.github.com>
2020-06-26 17:50:21 +0900

    Update backup_yourfile.py

--- backup_yourfile.py
+++ backup_yourfile.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*- 
+
+import os
+import time
+
+
 '''
 1. 백업
     1-1. 백업할 대상 : 1개 이상, 경로로 입력받는 모든 파일들
@@ -39,3 +45,61 @@
     5. 복원을 위해 백업할 파일을 선택한 문자열을 저장할 변수
     6. 
 '''
+# 사용자 입력값을 저장하기 위한 리스트
+target_folder_list = []
+
+# input 값을 source에 받는다.
+source = raw_input("Input your file path: \n");
+
+def write_into_file(source):
+    if not os.path.exists(source):
+        print "FILE PATH INPUT ERROR : please input existing file\n"
+    else:
+        # user_input.txt 파일을 추가하기 모드로 연 뒤, 입력한 파일 경로를 추가한다.
+        print "Appending your input file-paht into user_input.txt"
+        f = open('/home/nykim/user_input.txt', mode= 'at+')
+        f.write(source)
+        target_folder_list = list(f.readlines())
+        print "Loading..."
+        f.close()        
+
+
+
+
+# target_dir = '/home/nykim/backup'
+
+# # backup 파일이 없다면 그 위치에 만들어라.
+# if not os.path.exists(target_dir):
+#     os.mkdir(target_dir) # make directory
+
+# # 파일 이름 : year month date hour minute second
+# today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+# now = time.strftime('%H%M%S')
+
+
+# comment = raw_input('Enter a comment --> ')
+# # Check if a comment was entered
+
+# if len(comment) == 0:
+#     target = today + os.sep + now + '.zip'
+# else:
+#     target = today + os.sep + now + '_' + comment.replace(' ', '_') + '.zip'
+
+# # Create the subdirectory if it isn't already there
+# if not os.path.exists(today):
+#     os.mkdir(today)
+#     print 'Successfully created directory', today
+
+# # 5. We use the zip command to put the files in a zip archive
+# zip_command = "zip -r {0} {1}".format(target,
+#                                       ' '.join(source))
+
+# # Run the backup
+# print "Zip command is:"
+# print zip_command
+# print "Running:"
+# if os.system(zip_command) == 0:
+#     print 'Successful backup to', target
+# else:
+#     print 'Backup FAILED'
