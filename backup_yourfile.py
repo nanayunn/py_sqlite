d29c3e60132db9d74e83f2aa6aa253bb892fe025
nanayunn <58680504+nanayunn@users.noreply.github.com>
2020-06-29 00:38:26 +0900

    Update backup_yourfile.py

--- backup_yourfile.py
+++ backup_yourfile.py
@@ -46,6 +46,10 @@ import time
     6. 
 '''
 
+# -*- coding: utf-8 -*- 
+
+import os
+import time
 def get_file_path_input():
     source = raw_input("Input your file path: \n")  # input 값을 source에 받는다.
     target_folder_list = []  # 사용자 입력값을 저장하기 위한 리스트
@@ -57,46 +61,47 @@ def get_file_path_input():
         # user_input.txt 파일을 추가하기 모드로 연 뒤, 입력한 파일 경로를 추가한다.
         print "Appending your input file-pa into user_input.txt"
         f = open('/home/nykim/user_input.txt', mode= 'at+')
-        f.write(source)
         target_folder_list = list(f.readlines())
+        if source not in target_folder_list:
+            f.write(source)
+        else:
+            print "Already existing in file list.."
         print "Loading..."
         f.close()     
     return source, target_folder_list
 
+def get_backup_index(source, file_path):
+
+
+
 user_input = []
-
 user_input = get_file_path_input()
-source = user_input[0]
-target_folder_list = user_input[1]
+
+source = user_input[0] # file path
+target_folder_list = user_input[1] # 사용자가 그동안 백업하려고 입력한 파일들의 path
 
 
+backup_dir = '/home/nykim/backup' # 백업 디렉토리 위치
+# backup 파일이 없다면 그 위치에 만들어라.
+if not os.path.exists(backup_dir):
+    os.mkdir(backup_dir) # make directory
+
+backup_list = '/home/nykim/backup/backup_list.txt' # 백업 디렉토리 위치
+
+fid = open(backup_list, "w")
+if not os.path.isfile(backup_list):
+    fid.write("Backup Text file created.")
+ 
+fid.close()
 
 
+# 파일 이름 : year month date hour minute second
+today_backup = backup_dir + os.sep + time.strftime('%Y%m%d')
 
-# target_dir = '/home/nykim/backup'
-
-# # backup 파일이 없다면 그 위치에 만들어라.
-# if not os.path.exists(target_dir):
-#     os.mkdir(target_dir) # make directory
-
-# # 파일 이름 : year month date hour minute second
-# today = target_dir + os.sep + time.strftime('%Y%m%d')
-
-# now = time.strftime('%H%M%S')
-
-
-# comment = raw_input('Enter a comment --> ')
-# # Check if a comment was entered
-
-# if len(comment) == 0:
-#     target = today + os.sep + now + '.zip'
-# else:
-#     target = today + os.sep + now + '_' + comment.replace(' ', '_') + '.zip'
-
-# # Create the subdirectory if it isn't already there
-# if not os.path.exists(today):
-#     os.mkdir(today)
-#     print 'Successfully created directory', today
+# 백업을 진행하기 전, 백업을 실행하는 날짜를 기준으로 서브 디렉토리 생성
+if not os.path.exists(today_backup):
+    os.mkdir(today_backup)
+    print 'Successfully created directory', today_backup
 
 # # 5. We use the zip command to put the files in a zip archive
 # zip_command = "zip -r {0} {1}".format(target,
