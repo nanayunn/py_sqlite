ab9b13acfe7c8b70ab67ca47e3e1aadb29344e34
nanayunn <58680504+nanayunn@users.noreply.github.com>
2020-07-01 19:08:50 +0900

    Add files via upload

--- data_extract.py
+++ data_extract.py
@@ -1,80 +1,74 @@
 # -*- coding: utf-8 -*- 
+#!/usr/bin/python
 
-from redminelib import Redmine
-from redminelib.exceptions import AuthError
-import sqlite3
+from class_extraction import data_extraction
+from subprocess import call
+import os
+import time
 
-
-try:
-    redmine = Redmine('', key='',verify=False)
-except:
-    print("key validation Error. check API key.")
-
-issue2 = redmine.issue.filter(status_id = '*')
-
-
-filepath = "pask_issues.db"
-conn = sqlite3.connect(filepath)
-
-cur = conn.cursor()
-cur.execute("drop table if exists pask")
-
-cur.execute("""
-    create table pask(
-        project_name NOT NULL,
-        issues_id integer NOT NULL PRIMARY KEY,
-        trakcer_name text NOT NULL,
-        status_name text NOT NULL,
-        author_name text NOT NULL,
-        subject text NOT NULL,
-        created_date text NOT NULL
-    )
-""")
-conn.commit()
-
-for issue in issue2:
-    dict_issue = dict(issue)
-
-    project = dict_issue['project']
-    status = dict_issue['status']
-    tracker = dict_issue['tracker']
-    issues_id = dict_issue['id']
-    subject = dict_issue['subject']
-    created_date = dict_issue['created_on']
-    author = dict_issue['author']
-
-    project_name = project['name']
-    status_name = status['name']
-    trakcer_name = tracker['name']
-    author_name = author['name']
-
-    print project_name
-    print status_name
-    print trakcer_name
-    print issues_id
-    print created_date
-    print subject
-    print author_name
+def check_and_replace(filepath, mount_folder, final_file_path):
     
-    issue_list = []
+    if not os.path.exists(final_file_path):
+        try:
+            os.system('mv ' + filepath + ' ' + mount_folder) #temp 폴더 내의 issues.db를 mount 폴더 경로로 옮긴다.
+        except:
+            print "fail to mv file to folder\n"
+    elif os.path.exists(final_file_path):
+        try:
+            os.system('rm ' + final_file_path) # 마운트 폴더에 db 파일이 존재한다면 삭제
+            os.system('mv ' + filepath + ' ' + mount_folder) # 이후 폴더 내로 옮긴다.
+        except:
+            print "check_and_replace :check_and_replace error detected\n"
+            return -1
+    else:
+        print "check_and_replace : nothing happend. need to check up\n"
+        return -1
 
-    issue_list.append(project_name)
-    issue_list.append(issues_id)
-    issue_list.append(trakcer_name)
-    issue_list.append(status_name)  
-    issue_list.append(author_name)
-    issue_list.append(subject)
-    issue_list.append(created_date)
+    return 0
 
-    cur = conn.cursor()
-    cur.execute(
-            "insert into pask values (?,?,?,?,?,?,?)", issue_list
-           
-        )
-    conn.commit()
-conn.close()
+webfront_k = ''
+tifront = ''
+pas_k = ''
+
+data_base = ".db" #db 파일 이름
+
+temp_filepath = "" + data_base # bluerocket 인스턴스 내의 temp 폴더 위치
+
+mount_folder = "" # docker와 마운트 된 폴더의 위치
+
+final_file_path = mount_folder + os.sep + data_base
 
 
+date = time.localtime
+
+# class 객체 생성
+de = data_extraction()
+
+# 일감 추출할 제품의 사이트 이슈 페이지 주소 목록
+target_list = [webfront_k, tifront, pas_k]
+project_list = ['', '', '']
 
 
+# api key로 일감 추출할 제품을 각각 validation 체크
+redmine_list = de.is_valid(target_list)
 
+# validation이 체크되면 해당 target의 일감 정보에 접근할 수 있게 됨
+# 일감 정보를 (모든 상태의 일감)으로 필터링 후 각 target 별 issues를 추출
+issue_list = de.get_issues(redmine_list)
+
+# 각 프로젝트 별 이슈 목록을 딕셔너리 형태로 매칭
+each_issue_of_target = de.list_to_dict(project_list, issue_list)
+
+# 추출해서 각 프로젝트 별 테이블에 insert, conn 객체 반환
+last_conn = de.extract_data(temp_filepath, each_issue_of_target)
+
+# 객체를 닫아줌
+last_conn.close()
+
+
+result = check_and_replace(temp_filepath, mount_folder, final_file_path)
+
+if result is not 0:
+    print "error caused for moving file. please check up, wrong end.\n"
+else:
+    print "end"
