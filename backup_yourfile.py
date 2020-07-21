083b79ca542b6dfd2af3b7305e34afdb8204739d
nanayunn <58680504+nanayunn@users.noreply.github.com>
2020-07-21 17:44:19 +0900

    Add files via upload

--- backup_yourfile.py
+++ backup_yourfile.py
@@ -3,115 +3,147 @@
 import os
 import time
 
-
-'''
-1. 백업
-    1-1. 백업할 대상 : 1개 이상, 경로로 입력받는 모든 파일들
-
-    1-2. 백업 방법 : 파일 경로를 입력 받음.
-        1-2-1. 입력받은 경로에 위치한 파일을 'zip' 형식으로 압축하여 백업
-
-    1-3. 백업 리스트 : 리스트 형태로 인식
-        1-3-1. 사용자에게 값을 입력받아 저장하려면 txt 파일에 사용자가 입력한 경로를 따로 저장?
-
-    1-4. 백업 장소 : 'backup'이라는 디렉토리
-        1-4-1. zip 파일이 저장되는 위치
-
-    1-5. 백업 파일 이름 : 로컬타임에서 현재 시간을 불러와서 이름으로 지정.
-
-2. 복원
-    2-1. 복원할 대상 : 1개 이상
-        
-    2-2. 복원 방법 : 
-        2-2-1. 복원할 파일의 이름으로 복원?
-        2-2-2. 백업 파일을 저장할 때마다 백업 목록에 시간을 저장
-        2-2-3. 백업 목록을 리스트로 받아와서 출력
-        2-2-4. 사용자가 index + 1 값을 입력하면 해당 index값을 폴더의 이름과 비교, 복원 진행
-    
-    2-3. 복원 리스트 : 복원을 진행한 폴더와 날짜를 저장
-
-    2-4. 복원 장소 : 'rollback' 이라는 디렉토리
-
-    2-5. 복원 파일 이름 : rollback + 복원을 진행한 zip 파일의 이름
-
-3. 필요한 변수
-    1.  사용자가 입력한 파일 경로 (','로 구분됨)
-        1-1. txt, list
-    2. 백업을 진행한 파일 이름
-        2-1. txt, list
-    3. 복원을 진행한 파일 이름 및 복원 시간
-        3-1. txt
-    4. 사용자가 입력한 파일을 임시 저장할 변수
-    5. 복원을 위해 백업할 파일을 선택한 문자열을 저장할 변수
-    6. 
-'''
-
-# -*- coding: utf-8 -*- 
-
-import os
-import time
 def get_file_path_input():
     source = raw_input("Input your file path: \n")  # input 값을 source에 받는다.
-    target_folder_list = []  # 사용자 입력값을 저장하기 위한 리스트
+    try : 
+        if not os.path.exists(source):
+            print "FILE PATH INPUT ERROR : please input existing file\n"
 
-    if not os.path.exists(source):
-        print "FILE PATH INPUT ERROR : please input existing file\n"
-
-    else:
-        # user_input.txt 파일을 추가하기 모드로 연 뒤, 입력한 파일 경로를 추가한다.
-        print "Appending your input file-pa into user_input.txt"
-        f = open('/home/nykim/user_input.txt', mode= 'at+')
-        target_folder_list = list(f.readlines())
-        if source not in target_folder_list:
-            f.write(source)
         else:
-            print "Already existing in file list.."
-        print "Loading..."
-        f.close()     
-    return source, target_folder_list
+            print "Loading..."
+            print "Your target file is here : " + source
+    except Exception:
+        print "Exception\n"
 
-def get_backup_index(source, file_path):
+    return source
+    
+def rollback_file(backup_list):
+    
+    zipped_folder_list = []
+    index = 1
+    try: 
+        file = open(backup_list, "r+")
+        select_list = file.readlines()
+        file.close()
+
+        #로그 파일 내에서, 파일의 이름값만 빼온다.
+        for item in select_list:
+            item = item.split(',')
+            item = item[0]
+            print item
+            zipped_folder_list.append(item)
+        print zipped_folder_list
+            #사용자가 보고 선택할 수 있도록 프린트
+        print "==================select number=================="
+        for item2 in zipped_folder_list:
+            
+            print "folder number : " + str(index) , "|| zip file name: " + item2
+            index += 1
+    except:
+        print "error"
 
 
+    # 사용자에게 숫자로 리스트의 인덱스를 입력받는다. 범위값 예외 처리
+    user_input = raw_input("select number of folder you want to rollback : ")
+    if int(user_input) > index + 1 or int(user_input) < 1:
+        print index
+        print "out of range. start program again\n"
+    else:
+        # index는 1부터 시작하므로 실제로 사용자가 입력한 숫자의 리스트 index는 index -1 
+        target_zip = zipped_folder_list[int(user_input) - 1]
+        print target_zip
+        if os.path.exists(target_zip):
+            print "found zip file"
+            # unzip한 파일을 위치시킬 폴더 위치를 입력 받는다.
+            place_to_rollback = raw_input("input file name where you want to place unzipped file : ")
 
-user_input = []
-user_input = get_file_path_input()
+            #실행할 unzip 명령어
+            unzip_command = "unzip {0} -d {1}".format(target_zip,''.join(place_to_rollback))
+                    
+            #unzip 실행
+            print "Unzip command is:"
+            print unzip_command
+            print "Running:"
+            if os.system(unzip_command) == 0:
+                print 'Successful unzip to', place_to_rollback
+            else:
+                print 'Unzip FAILED'
+        else:
+            print "none"
 
-source = user_input[0] # file path
-target_folder_list = user_input[1] # 사용자가 그동안 백업하려고 입력한 파일들의 path
+#============== zip 파일 생성 ==============#
 
+source = get_file_path_input()
+parse_source = source.split('/')
+new_source = []
+for source2 in parse_source:
+    if source2 is '':
+        pass
+    else:
+        new_source.append(source2)
+parse_source = new_source[-1]
+print new_source
+print parse_source
 
 backup_dir = '/home/nykim/backup' # 백업 디렉토리 위치
 # backup 파일이 없다면 그 위치에 만들어라.
 if not os.path.exists(backup_dir):
     os.mkdir(backup_dir) # make directory
 
-backup_list = '/home/nykim/backup/backup_list.txt' # 백업 디렉토리 위치
-
-fid = open(backup_list, "w")
-if not os.path.isfile(backup_list):
-    fid.write("Backup Text file created.")
- 
-fid.close()
-
+print backup_dir
 
 # 파일 이름 : year month date hour minute second
-today_backup = backup_dir + os.sep + time.strftime('%Y%m%d')
+backup_day = time.strftime('%Y%m%d')
+
+today_backup = backup_dir + os.sep + backup_day
 
 # 백업을 진행하기 전, 백업을 실행하는 날짜를 기준으로 서브 디렉토리 생성
 if not os.path.exists(today_backup):
     os.mkdir(today_backup)
     print 'Successfully created directory', today_backup
 
-# # 5. We use the zip command to put the files in a zip archive
-# zip_command = "zip -r {0} {1}".format(target,
-#                                       ' '.join(source))
+
+# 백업하는 파일 경로 + 백업할 당시의 시간대를 zip 파일의 이름으로 설정
+rightnow = time.strftime('%Y%m%d%H%M%S')
+backup_file_name = today_backup + os.sep + rightnow + '_' + parse_source + '.zip'
+print backup_file_name
+# # # 5. -r 옵션은 해당 폴더 경로의 하위 폴더까지 모두 합쳐서 압축한다는 옵션
+zip_command = "zip -r {0} {1}".format(backup_file_name,''.join(source))
+print "source :" + source
 
 # # Run the backup
-# print "Zip command is:"
-# print zip_command
-# print "Running:"
-# if os.system(zip_command) == 0:
-#     print 'Successful backup to', target
-# else:
-#     print 'Backup FAILED'
+print "Zip command is:"
+print zip_command
+print "Running:"
+if os.system(zip_command) == 0:
+    print 'Successful backup ', source
+else:
+    print 'Backup FAILED'
+
+# #============== rollback을 위한 코드  ==============#
+
+
+# zip 파일 생성을 실행할 때마다, 동시에 zip 파일로 생성된 백업 파일의 이름을 txt 파일에 저장한다.
+backup_list = '/home/nykim/backup/backup_created_log.txt' 
+
+if not os.path.exists(backup_list):
+    try: 
+        file = open(backup_list, "wt+") # 파일이 없다면 생성
+        print backup_file_name
+        file.write(backup_file_name + ",Backup Text file created.\n")
+        file.close()
+    except Exception:
+        print "backup created log file error"
+
+else:
+    try: 
+        file = open(backup_list, "at+") #파일이 있다면 append 모드로 설정
+        file.write(backup_file_name + ",Backup Text file created.\n")
+        print backup_file_name
+        file.close()
+
+    except Exception:
+        print "backup created log file error"
+
+
+rollback_file(backup_list)
